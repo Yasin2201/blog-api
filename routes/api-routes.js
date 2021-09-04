9c72cf93916d8484e76bf9a1ba6ba85053fc58e1
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-09-04 20:02:11 +0100

    added route for sign-in

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,4 +6,7 @@ const user_controller = require('../controllers/userController');
 //POST user Sign-Up form
 router.post('/sign-up', user_controller.sign_up_post)
 
+//POST user sign-in 
+router.post('/sign-in', user_controller.sign_in_post)
+
 module.exports = router

a67d1b91e2453f1f888762b89b86436e5da4378d
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-09-05 16:48:32 +0100

    added post UPDATE to postController

--- controllers/postController.js
+++ controllers/postController.js
@@ -33,19 +33,48 @@ exports.new_post = [
     }
 ];
 
+//Update post
+exports.update_post = [
+
+    body('author', 'Author Must Not Be Empty').trim().isLength({ min: 1 }).escape(),
+    body('title', 'Title Must Not Be Empty').trim().isLength({ min: 1 }).escape(),
+    body('text', 'Text Must Not Be Empty').trim().isLength({ min: 1 }).escape(),
+
+    (req, res, next) => {
+        //Errors from req if any
+        const errors = validationResult(req);
+
+        const { author, title, text } = req.body;
+        const post = new Post({
+            _id: req.params.id,
+            author,
+            title,
+            text
+        });
+
+        if (!errors.isEmpty()) {
+            //re-render form if any errors
+            res.json({ errors: errors.array() })
+            return
+        } else {
+            // successful redirect to update category detail page
+            Post.findByIdAndUpdate(req.params.id, post, {}, function (err) {
+                if (err) { return next(err) }
+                res.json({
+                    message: "Post Updated",
+                    post
+                });
+            });
+        };
+    }
+];
+
 //Delete post
 exports.delete_post = function (req, res, next) {
-    Post.findById(req.params.id)
-        .exec(function deletePost(err, foundPost) {
-            if (err) {
-                return next(err)
-            } else {
-                Post.remove({ _id: req.params.id }, function (err) {
-                    if (err) { return next(err) }
-                    res.json({
-                        message: "Successfully Deleted Post",
-                    })
-                })
-            }
-        })
-}
+    Post.findByIdAndRemove(req.params.id, function deletePost(err) {
+        if (err) { return next(err) }
+        res.json({
+            message: "Successfully Deleted Post",
+        });
+    });
+};
